c264106b79bc88ce49c5c4c6b592c718b8268a65
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-07-29 14:40:54 +0200

    Aggiungere i file di progetto.

---
+++ LaPizzeria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaPizzeria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
